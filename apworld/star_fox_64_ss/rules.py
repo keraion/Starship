1c7f1e07d2a20e6db330f65b35b091370d638c8e
Cameron <105471409+keraion@users.noreply.github.com>
2026-09-22 23:33:52 -0400

    fix: any_except rule len check bug

--- apworld/star_fox_64_ss/rules.py
+++ apworld/star_fox_64_ss/rules.py
@@ -126,7 +126,7 @@ class StarFox64SSRules(ast.NodeTransformer):
     if (isinstance(node.func, ast.Name)):
       match node.func.id:
         case "any_except":
-          assert len(node.args >= 2), f"{self.file}: any_except requires at least 2 arguments."
+          assert len(node.args) >= 2, f"{self.file}: any_except requires at least 2 arguments."
           except_list = []
           for idx, arg in enumerate(node.args):
             arg = self.visit(arg)
@@ -140,7 +140,7 @@ class StarFox64SSRules(ast.NodeTransformer):
             for elt in elts:
               assert isinstance(elt, ast.Constant), f"{self.file}: any_except invalid arg: {idx}"
               except_list.append(elt.value)
-          assert isinstance(node, ast.Call) or isinstance(node.args[0], ast.Tuple), f"{self.file}: any_except requires arg 1 to be a list of items."
+          assert isinstance(node, ast.Call) and isinstance(node.args[0], ast.Tuple), f"{self.file}: any_except requires arg 1 to be a list of items."
           node.args[0].elts = list(filter(lambda elt: elt.value not in except_list, node.args[0].elts))
           return node
         case "count":
