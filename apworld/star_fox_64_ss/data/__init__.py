d6880f89fcc346deaf6740738d7e206d6f9e2660
Cameron <105471409+keraion@users.noreply.github.com>
2026-09-23 10:11:43 -0400

    drop extra_regions

--- apworld/star_fox_64_ss/data/__init__.py
+++ apworld/star_fox_64_ss/data/__init__.py
@@ -1,4 +1,5 @@
 import importlib
+
 from . import Game
 from .Game import items
 
@@ -6,8 +7,7 @@ regions = {}
 
 for module_name in Game.region_modules:
     module = importlib.import_module(f".{module_name}", __name__)
-    if hasattr(module, "extra_regions"):
-        Game.region_modules += [f"{module_name}.{extra}" for extra in module.extra_regions]
     for region_name, region in module.regions.items():
-        if region_name in regions: raise Exception(f"Duplicated Region from {module_name}: '{region_name}'")
+        if region_name in regions:
+            raise Exception(f"Duplicated Region from {module_name}: '{region_name}'")
         regions[region_name] = region
